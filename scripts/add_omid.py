791f014aaabcd71f7d657232700f29a052ac309a
Vikram Ramavarapu <vikramr2@illinois.edu>
2024-06-12 21:08:01 -0500

    reorg

--- scripts/add_omid.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Reading the CSV content
-df = pd.read_csv('ocmeta_last_5yrs.csv')
-
-# Adding the 'omid' column by extracting the desired part from the 'id' column
-df['omid'] = df['id'].apply(lambda x: ' '.join([part for part in x.split() if part.startswith('omid:br/')]))
-
-df.to_csv('ocmeta_last_5yrs.csv', index=False)
